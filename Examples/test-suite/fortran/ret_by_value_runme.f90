0a7853dc751a4a2d9f51023b0dd44bb264517f9a
Mitchell T.H. Young [y4o] <y4o@yupana.ornl.gov>
2017-04-28 14:06:08 -0400

    Fix Makefile.in for Fortran test-suite

--- Examples/test-suite/fortran/ret_by_value_runme.f90
+++ Examples/test-suite/fortran/ret_by_value_runme.f90
@@ -2,23 +2,31 @@ program ret_by_value_runme
 
   use ret_by_value
   use iso_fortran_env
+  use iso_c_binding
 
   type(test) :: test_val
   logical :: okay
+  integer(c_short) :: short
+  integer(c_int) :: int
 
   okay = .true.
 
   test_val = get_test()
 
-  if(test_val%myInt /= 100) then
+  int = test_val%get_myInt()
+  short = test_val%get_myShort()
+
+  if(test_val%get_myInt() /= 100) then
     write(*,*)"Wrong value for %myInt",100
     okay = .false.
+  endif
+
+  if(test_val%get_myShort() /= 200) then
+    write(*,*)"Wrong value for "
   endif
 
 
   if(.not.okay) then
     stop 1
   endif
-
-
 end program
