32fd07b2f10f7eefb788dd2045b785b0903bff0a
Seth R Johnson <johnsonsr@ornl.gov>
2018-01-17 14:41:12 -0500

    Reinstating reinterpret_casts to fix unit tests
    I'm not sure this is the *correct* approach: the reinterpret_casts might
    not be valid.

--- Examples/test-suite/fortran/failing-tests.mk
+++ Examples/test-suite/fortran/failing-tests.mk
@@ -1,7 +1,6 @@
 
 # failed to compile wrapper
 FAILING_CPP_TESTS += \
-  apply_strings \
   array_member \
   arrays_global \
   arrays_global_twodim \
@@ -14,7 +13,6 @@ FAILING_CPP_TESTS += \
   director_enum \
   enum_thorough \
   global_vars \
-  li_std_vector \
   li_std_vector_enum \
   member_funcptr_galore \
   mixed_types \
@@ -26,17 +24,10 @@ FAILING_CPP_TESTS += \
 # failed to compile fortran
 FAILING_CPP_TESTS += \
   abstract_signature \
-  abstract_typedef2 \
-  arrays_dimensionless \
   arrays_scope \
-  bloody_hell \
-  class_ignore \
-  class_scope_weird \
   constructor_copy \
   contract \
   cpp_basic \
-  cpp_typedef \
-  derived_nested \
   director_basic \
   director_classes \
   director_default \
@@ -46,28 +37,15 @@ FAILING_CPP_TESTS += \
   director_keywords \
   director_overload2 \
   director_property \
-  enum_ignore \
   enum_scope_template \
   evil_diamond_prop \
   extend \
   extend_default \
-  friends_template \
   fvirtual \
-  global_namespace \
-  global_scope_types \
-  inherit_missing \
   inherit_target_language \
-  kind \
   li_attribute \
   li_attribute_template \
   li_boost_shared_ptr \
-  li_boost_shared_ptr_bits \
-  li_cdata_cpp \
-  li_std_vector_ptr \
-  li_typemaps \
-  li_typemaps_apply \
-  li_windows \
-  long_long_apply \
   member_pointer_const \
   multiple_inheritance_abstract \
   multiple_inheritance_interfaces \
@@ -75,75 +53,45 @@ FAILING_CPP_TESTS += \
   multiple_inheritance_shared_ptr \
   namespace_class \
   namespace_nested \
-  namespace_typemap \
-  namespace_virtual_method \
-  naturalvar_more \
-  nested_class \
-  nested_ignore \
   nested_scope \
   nested_workaround \
   operator_overload \
   operator_overload_break \
   overload_arrays \
   overload_method \
-  overload_polymorphic \
   overload_return_type \
   overload_simple \
   overload_subtype \
   overload_template \
   overload_template_fast \
   pod_struct \
-  primitive_ref \
   pure_virtual \
   refcount \
-  register_par \
   return_const_value \
-  sizet \
   smart_pointer_const_overload \
-  smart_pointer_ignore \
   smart_pointer_inherit \
   smart_pointer_rename \
   smart_pointer_template_const_overload \
   smart_pointer_template_defaults_overload \
   special_variables \
-  template_arg_scope \
   template_arg_typename \
   template_basic \
-  template_default \
   template_default_arg \
-  template_default_vw \
   template_enum \
   template_explicit \
   template_extend_overload \
-  template_forward \
   template_inherit_abstract \
   template_methods \
-  template_nested \
   template_ns \
-  template_ns2 \
-  template_ns_enum \
-  template_ns_enum2 \
-  template_opaque \
   template_ref_type \
-  template_retvalue \
-  template_typedef_class_template \
-  template_typedef_cplx5 \
   template_typedef_inherit \
-  template_typemaps \
-  template_typemaps_typedef \
-  template_typemaps_typedef2 \
-  template_using_directive_and_declaration_forward \
-  template_whitespace \
-  typemap_global_scope \
   typemap_ns_using \
-  typemap_template_parm_typedef \
   typemap_various \
   using1 \
   using2 \
   using_inherit \
   using_protected \
   valuewrapper_base \
-  valuewrapper_opaque \
   variable_replacement \
   virtual_poly \
   wallkw
