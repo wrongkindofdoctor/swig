ba8055d7c5e2096cc27f410d65218211a8e5b418
Seth R Johnson <johnsonsr@ornl.gov>
2018-01-22 10:39:16 -0500

    fixup! Fix assignment under C++98

--- Examples/test-suite/fortran/failing-tests.mk
+++ Examples/test-suite/fortran/failing-tests.mk
@@ -1,21 +1,105 @@
 
+# failed to compile wrapper
+FAILING_CPP_TESTS += \
+  abstract_access \
+  abstract_basecast \
+  abstract_inherit \
+  abstract_inherit_ok \
+  abstract_signature \
+  abstract_typedef \
+  abstract_typedef2 \
+  abstract_virtual \
+  allprotected \
+  default_constructor \
+  destructor_reprotected \
+  director_abstract \
+  director_binary_string \
+  director_constructor \
+  director_default \
+  director_detect \
+  director_frob \
+  director_ignore \
+  director_nested \
+  director_pass_by_value \
+  director_protected \
+  director_protected_overloaded \
+  director_using \
+  global_scope_types \
+  inherit_void_arg \
+  li_attribute_template \
+  li_boost_shared_ptr \
+  li_boost_shared_ptr_bits \
+  li_std_combinations \
+  li_std_except_as_class \
+  li_std_map \
+  li_std_pair \
+  li_std_pair_using \
+  member_pointer \
+  member_pointer_const \
+  multiple_inheritance_abstract \
+  multiple_inheritance_nspace \
+  multiple_inheritance_shared_ptr \
+  name_warnings \
+  namespace_virtual_method \
+  nested_directors \
+  nested_scope \
+  private_assign \
+  pure_virtual \
+  refcount \
+  smart_pointer_inherit \
+  smart_pointer_protected \
+  smart_pointer_template_defaults_overload \
+  special_variable_macros \
+  special_variables \
+  swig_exception \
+  template_arg_replace \
+  template_arg_typename \
+  template_base_template \
+  template_default \
+  template_default2 \
+  template_default_arg_overloaded_extend \
+  template_default_arg_virtual_destructor \
+  template_default_class_parms \
+  template_default_class_parms_typedef \
+  template_enum_ns_inherit \
+  template_forward \
+  template_inherit_abstract \
+  template_ns \
+  template_ns4 \
+  template_ns_inherit \
+  template_partial_specialization \
+  template_partial_specialization_typedef \
+  template_specialization_defarg \
+  template_specialization_enum \
+  template_tbase_template \
+  template_template_parameters \
+  template_typedef \
+  template_typedef_class_template \
+  template_typedef_cplx \
+  template_typedef_cplx2 \
+  template_typedef_cplx3 \
+  template_typedef_cplx4 \
+  template_typedef_funcptr \
+  template_typedef_ptr \
+  template_typemaps_typedef \
+  template_typemaps_typedef2 \
+  template_virtual \
+  using_protected \
+  virtual_destructor \
+  virtual_poly \
+  virtual_vs_nonvirtual_base
+
 # failed to compile fortran
 FAILING_CPP_TESTS += \
-  abstract_signature \
   arrays_scope \
   bloody_hell \
   char_strings \
   constructor_copy \
   contract \
   cpp_basic \
-  default_constructor \
   director_basic \
   director_classes \
-  director_default \
-  director_detect \
   director_enum \
-  director_frob \
-  director_ignore \
   director_keywords \
   director_overload2 \
   director_property \
@@ -27,19 +111,11 @@ FAILING_CPP_TESTS += \
   fvirtual \
   inherit_target_language \
   li_attribute \
-  li_attribute_template \
-  li_boost_shared_ptr \
-  member_pointer_const \
-  multiple_inheritance_abstract \
   multiple_inheritance_interfaces \
-  multiple_inheritance_nspace \
-  multiple_inheritance_shared_ptr \
   namespace_class \
   namespace_nested \
-  nested_scope \
   nested_workaround \
   operator_overload \
-  operator_overload_break \
   overload_arrays \
   overload_method \
   overload_return_type \
@@ -47,24 +123,16 @@ FAILING_CPP_TESTS += \
   overload_subtype \
   overload_template \
   overload_template_fast \
-  pure_virtual \
-  refcount \
   return_const_value \
   smart_pointer_const_overload \
-  smart_pointer_inherit \
   smart_pointer_rename \
   smart_pointer_template_const_overload \
-  smart_pointer_template_defaults_overload \
-  special_variables \
-  template_arg_typename \
   template_basic \
   template_default_arg \
   template_enum \
   template_explicit \
   template_extend_overload \
-  template_inherit_abstract \
   template_methods \
-  template_ns \
   template_ref_type \
   template_typedef_inherit \
   typemap_ns_using \
@@ -72,8 +140,6 @@ FAILING_CPP_TESTS += \
   using1 \
   using2 \
   using_inherit \
-  using_protected \
   valuewrapper_base \
   variable_replacement \
-  virtual_poly \
   wallkw
