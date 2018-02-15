fd567be12240b8dfe8da9ed577682840f8c232bc
Seth R Johnson <johnsonsr@ornl.gov>
2018-02-15 06:52:19 -0500

    Exclude failing C tests

---
+++ Examples/test-suite/fortran/failing-c-tests.mk
@@ -0,0 +1,18 @@
+
+# failed to run swig
+FAILING_C_TESTS += \
+  li_carrays \
+  li_cpointer \
+  nested_extend_c \
+  overload_extend2 \
+  overload_extend_c
+
+# failed to compile wrapper
+FAILING_C_TESTS += \
+  integers \
+  li_constraints
+
+# failed to compile fortran
+FAILING_C_TESTS += \
+  enum_forward \
+  preproc
--- Examples/test-suite/fortran/process-failures.py
+++ Examples/test-suite/fortran/process-failures.py
@@ -5,22 +5,40 @@
 from collections import defaultdict
 import os.path
 
-BUILD_DIR = "/Users/s3j/_code/_build/swig-debug"
+class Results(object):
+    BUILD_DIR = "/Users/s3j/_code/_build/swig-debug"
 
-results = defaultdict(list)
-with open(os.path.join(BUILD_DIR,
-        "Examples/test-suite/fortran/summary.txt")) as f:
-    for line in f:
-        (test, result) = line.strip().split('|')
-        results[result].append(test)
+    def __init__(self, path):
+        self.results = results = defaultdict(list)
+        with open(os.path.join(self.BUILD_DIR, path)) as f:
+            for line in f:
+                (test, result) = line.strip().split('|')
+                results[result].append(test)
 
-for (k,v) in results.items():
-    print("{:25s}: {:4d}".format(k, len(v)))
+    def keys(self):
+        return reversed(sorted(self.results))
 
-with open("failing-tests.mk", 'w') as f:
-    for k in reversed(sorted(results)):
-        if k == "passed":
-            continue
-        print('\n#',k, file=f)
-        print("FAILING_CPP_TESTS += \\\n ", " \\\n  ".join(sorted(results[k])),
-                file=f)
+    def __str__(self):
+        lines = []
+        for k in self.keys():
+            lines.append("{:25s}: {:4d}".format(k, len(self.results[k])))
+        return "\n".join(lines)
+
+    def dump(self, f, failtxt="FAILING_CPP_TESTS"):
+        results = self.results
+        for k in self.keys():
+            if k == "passed":
+                continue
+            print('\n#',k, file=f)
+            print(failtxt + " += \\\n ", " \\\n  ".join(sorted(results[k])),
+                  file=f)
+
+#cppresults = Results("Examples/test-suite/fortran/summary.txt")
+#print(cppresults)
+#with open("failing-tests.mk", 'w') as f:
+#    cppresults.dump(f)
+
+cresults = Results("Examples/test-suite/fortran/summary-c.txt")
+print(cresults)
+with open("failing-c-tests.mk", 'w') as f:
+    cresults.dump(f, "FAILING_C_TESTS")
